8b9e47613d2b71967a24b7e2728bd937a8a56f14
Ben Moretti <benmoretti@icloud.com>
2021-06-04 22:53:00 +0930

    initial commit

---
+++ intersections.R
@@ -0,0 +1,136 @@
+#' intersections.R
+#' 
+#' Plots for the intersections around Fullarton / GO Road 
+#' 
+#'
+#' Data Source
+#' 
+#' https://data.sa.gov.au/data/dataset/traffic-volumes-on-top-40-intersections-in-sa
+#' 
+#' 4 June 2021
+#' 
+#' 
+
+# Libraries ---------------------------------------------------------------------------------------------------------
+
+
+library(tidyverse)
+library(janitor)
+library(tidyquant)
+library(scales)
+
+
+# Data Gathering ----------------------------------------------------------------------------------------------------
+
+# All of the data have been downloaded to the data directory from the repository above 
+
+# 2017 data
+data_2017_tbl <- read_csv(file = "data/top-40-at-grade-arterial-road-intersections-for-all-vehicles-2017.csv", skip=2) %>%
+  clean_names() %>%
+  mutate(
+    recorded_year = 2017
+  )
+
+# 2018 data 
+data_2018_tbl <- read_csv(file = "data/top-40-at-grade-arterial-road-intersections-for-all-vehicles-2018.csv", skip=2) %>%
+  clean_names() %>%
+  mutate(
+    recorded_year = 2018
+  )
+
+# 2019 data
+data_2019_tbl <- read_csv(file = "data/top-40-at-grade-arterial-road-intersections-for-all-vehicles-2019.csv", skip=2) %>%
+  clean_names() %>%
+  mutate(
+    recorded_year = 2019
+  )
+
+# 2020 data 
+data_2020_tbl <- read_csv(file = "data/top-40-at-grade-arterial-road-intersections-for-all-vehicles-2020.csv", skip=2) %>%
+  clean_names() %>%
+  mutate(
+    recorded_year = 2020
+  )
+
+# Tidy ----------------------------------------------------------------------------------------------
+
+# combine the data into a single data frame
+data_combined_tbl <- bind_rows(data_2017_tbl,
+                               data_2018_tbl,
+                               data_2019_tbl,
+                               data_2020_tbl)
+
+#data_combined_tbl <- data_combined_tbl %>%
+#  mutate(recorded_year = make_date(year=recorded_year))
+
+
+# Explore -------------------------------------------------------------------------------------------
+
+# data frame structure
+data_combined_tbl %>% glimpse()
+
+# look at the distinct names for the location
+data_combined_tbl %>%
+  distinct(location) %>%
+  arrange(location) %>%
+  View()
+
+# so looks like we just need the following locations 
+data_combined_tbl %>%  
+  filter(str_detect(location, "GLEN OSMOND") | str_detect(location, "FULLARTON") | str_detect(location, "UNLEY") | str_detect(location, "PORTRUSH"))  %>%
+  filter(! str_detect(location, "PAYNEHAM")) %>%
+  distinct(location) 
+ 
+# assign the locations we want to a list 
+location_lst <- data_combined_tbl %>%  
+  filter(str_detect(location, "GLEN OSMOND") | str_detect(location, "FULLARTON") | str_detect(location, "UNLEY") | str_detect(location, "PORTRUSH"))  %>%
+  filter(! str_detect(location, "PAYNEHAM")) %>%
+  distinct(location) %>%
+  pull()
+ 
+# Visualise -----------------------------------------------------------------------------------------------------
+
+# plot out the data in time series 
+
+data_combined_tbl %>%  
+  
+  # only get the required locations
+  filter(location %in% locations_lst) %>% 
+  
+  # clean up location so that it is a bit tidier and shorter 
+  mutate(
+    location_text = str_remove_all(location, " ROAD"),
+    location_text = str_trim(location, side="both")
+  ) %>%
+  
+  # plot a line plot with points for the recorded year vs vehicle exposure with colour by location and a label of the number
+  ggplot(aes(x=recorded_year, y=vehicle_exposure, colour=location_text, label=vehicle_exposure)) +
+  geom_line(size=2) +
+  geom_point(size=3) +
+  geom_text(aes(label=vehicle_exposure),hjust=0, vjust=2) +
+  
+  # facet wrap by location
+  facet_wrap(vars(location_text)) +
+  ylim(0,80000) +
+  
+  # theme stuff
+  theme_tq() +
+  scale_colour_tq() +
+  
+  # informative labels
+  labs(
+    title="Intersection Traffic Volumes in or around Glen Osmond Road",
+    subtitle = str_glue("Derived from \"Traffic volumes on top 40 intersections in SA\". 
+    
+                         Source: https://data.sa.gov.au/data/dataset/traffic-volumes-on-top-40-intersections-in-sa
+                         
+                         Vehicle Exposure: A measure of traffic volumes passing through intersections. It is calculated by adding the estimated Annual Average Daily Traffic (est. AADT) volumes on all arms of the intersection 
+                         and dividing the result by two. Vehicle Exposure estimates are based on data obtained from the most recent single day 11 hour manual turning count surveys conducted at the intersections."),
+    x = "Recorded Year",
+    y = "Vehicle Exposure",
+    caption = "4 June 2021, Ben Moretti @morebento",
+    colour = "Location"
+  )
+  
+
+
